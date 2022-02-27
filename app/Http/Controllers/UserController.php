f96d603425657aee6e95f2a075874a61dd156613
Keyvan <k.poursoltani@outlook.com>
2022-02-28 01:18:45 +0330

    signup functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use \App\Models\User;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
@@ -25,5 +27,25 @@ class UserController extends Controller
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ]);
+    }
+
+    public function signup_page(Request $request)
+    {
+        return view('signup');
+    }
+
+    public function signup(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email' , 'unique:users'],
+            'password' => ['required' , Password::min(8)],
+            'name' =>['required' , 'min:8' , 'unique:users']
+        ]);
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return redirect()->route('home');
     }
 }

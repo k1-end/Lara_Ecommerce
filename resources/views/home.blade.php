4c180483ce5472386ecbb3138d4845f4b1e086af
Keyvan <k.poursoltani@outlook.com>
2022-04-16 23:20:26 +0430

    Slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.master')
 
 @section('content')
+<section class="splide" aria-label="Splide Basic HTML Example">
+    <style>
+        
+    </style>
+    <div class="splide__track">
+        @php
+            $slides = $products->random(4);
+        @endphp
+          <ul class="splide__list">
+              @foreach ($slides as $slide)
+              <li class="splide__slide"><a href="{{route('product',['product' => $slide])}}"><img src="{{Storage::url($slide->thumbnail)}}" alt="" ></a></li>
+              @endforeach
+          </ul>
+    </div>
+  </section>
     @if($products)
         <ul class="d-flex flex-wrap" >
         @foreach($products as $p)
@@ -14,5 +29,13 @@
         </ul>
         {{$products->links()}}
     @endif
-
+    <script>
+        new Splide( '.splide' , {
+            type : 'loop',
+            // rewind : true,
+            focus: 'center',
+            // autoplay : true,
+            // interval : 1000
+        }).mount();
+      </script>
 @endsection

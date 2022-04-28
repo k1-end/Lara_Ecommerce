1dc07c626c30f3dd178564df26c756cae57cf1b0
Keyvan <k.poursoltani@outlook.com>
2022-04-28 20:03:31 +0430

    Image column added on Products table

--- database/migrations/2022_02_27_145538_create_products_table.php
+++ database/migrations/2022_02_27_145538_create_products_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
 	    $table->string('category');
 	    $table->string('price');
 	    $table->string('thumbnail');
+        $table->string('image');
 	    $table->softDeletes();
         });
     }
